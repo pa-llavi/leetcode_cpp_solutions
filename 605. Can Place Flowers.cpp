f2e0a1d9d3b2021817c97d277242aa457f449f81
pa-llavi <59191317+pa-llavi@users.noreply.github.com>
2021-03-13 17:46:20 +0530

    Create 605. Can Place Flowers.cpp

---
+++ 605. Can Place Flowers.cpp
@@ -0,0 +1,39 @@
+/********************************************* SOLUTION 1 **************************************************************************/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+         int i = 0, count = 0;
+        while (i < flowerbed.size()) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            i++;
+        }
+        return count >= n;
+    }
+};
+
+
+
+/********************************************* SOLUTION 2 (optimized) **************************************************************************/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+         int i = 0, count = 0;
+        while (i < flowerbed.size()) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+            
+            if(count >= n){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+};
