af0dc552a00d2f24c0d59f2d48e55dde4657518c
pa-llavi <59191317+pa-llavi@users.noreply.github.com>
2021-03-13 16:41:16 +0530

    Create 1437. Check If All 1's Are at Least Length K Places Away.cpp

---
+++ 1437. Check If All 1's Are at Least Length K Places Away.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int count = k;
+        
+        for(int num : nums) {
+            if(num == 1) {
+                if(count < k){
+                    return false;
+                }
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+        return true;
+    }
+};
